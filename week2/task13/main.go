173966b0f1e7f8a2affb52f3004cf04ec1b4c040
malov.rrc <malov.rrc@yandex.ru>
2026-08-25 00:31:11 +0400

    refactor: task13 refactored

--- week2/task13/main.go
+++ week2/task13/main.go
@@ -2,41 +2,19 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"task13/models"
 )
 
-// **13. Сортировка через `sort.Slice`**
-// `type Person struct { Name string; Age int }`. Хардкод: `{"Anna",34}, {"Boris",22}, {"Vera",45}, {"Gleb",22}`.
-// Сортировать по `Age` **по убыванию**; при равном `Age` — по `Name` **по возрастанию** (детерминированность).
-// Напечатать список до и после сортировки, построчно: `Имя (Возраст)`.
-type Person struct {
-	Name string
-	Age  int
-}
-
 func main() {
-	persons := []Person{
-		{"Anna", 34},
-		{"Boris", 22},
-		{"Vera", 45},
-		{"Gleb", 22},
+	persons := []models.Person{
+		{Name: "Anna", Age: 34},
+		{Name: "Boris", Age: 22},
+		{Name: "Vera", Age: 45},
+		{Name: "Gleb", Age: 22},
 	}
-	fmt.Println("Before sort: ")
-	for i := range persons {
-		fmt.Printf("%s (%d)\n", persons[i].Name, persons[i].Age)
-	}
-	sortPersons(persons)
-	fmt.Println("After sort: ")
-	for i := range persons {
-		fmt.Printf("%s (%d)\n", persons[i].Name, persons[i].Age)
-	}
-}
-
-func sortPersons(persons []Person) {
-	sort.Slice(persons, func(i, j int) bool {
-		if persons[i].Age == persons[j].Age {
-			return persons[i].Name < persons[j].Name
-		}
-		return persons[i].Age > persons[j].Age
-	})
+	fmt.Println("--------------------- Before sort: ---------------------")
+	models.PrintPersons(persons)
+	models.SortPersons(persons)
+	fmt.Println("--------------------- After sort: ---------------------")
+	models.PrintPersons(persons)
 }
