1a60b1b3baa9fa22135587b007168e556dfa521f
malov.rrc <malov.rrc@yandex.ru>
2026-08-31 21:08:50 +0400

    fix: deleted ai artifacts

--- week2/books_catalog/main.go
+++ week2/books_catalog/main.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"books_catalog/models"
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
 )
 
 type Book = models.Book
@@ -15,41 +11,5 @@ func parseLine(line string) (Book, error) {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "error: file path is required")
-		os.Exit(1)
-	}
 
-	filename := os.Args[1]
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	var books []Book
-	invalidCount := 0
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.TrimSpace(line) == "" {
-			continue
-		}
-
-		book, err := parseLine(line)
-		if err != nil {
-			invalidCount++
-			continue
-		}
-		books = append(books, book)
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintf(os.Stderr, "error reading file: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Print(models.FormatCatalog(books, invalidCount))
 }
