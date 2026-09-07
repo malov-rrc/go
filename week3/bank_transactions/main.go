a4c2392997b867c5d455e30537c0d48027bdd570
malov.rrc <malov.rrc@yandex.ru>
2026-09-08 00:04:05 +0400

    feat: added core functionality for bank_transactions

--- week3/bank_transactions/main.go
+++ week3/bank_transactions/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"bank_transactions/models"
+	"bank_transactions/utils"
+	"flag"
+	"fmt"
+	"os"
+)
 
+func main() {
+	//if len(os.Args) < 2 {
+	//	fmt.Fprintln(os.Stderr, "укажите путь к файлу")
+	//	os.Exit(1)
+	//}
+	account := models.Account{Balance: 0}
+	fileName := flag.String("file", "transactions.txt", "текстовый файл с транзакциями")
+	flag.Parse()
+	file := utils.OpenFile(*fileName)
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "ошибка при закрытии файла")
+			os.Exit(1)
+		}
+	}(file)
+	fileLines := utils.GetNotEmptyFileLines(file)
+	fmt.Println(account.MakeTransactions(fileLines))
 }
--- week3/bank_transactions/models/account.go
+++ week3/bank_transactions/models/account.go
@@ -1,6 +1,11 @@
 package models
 
-import "errors"
+import (
+	"bank_transactions/utils"
+	"errors"
+	"fmt"
+	"strings"
+)
 
 type Account struct {
 	Balance float64
@@ -22,4 +27,59 @@ func (a *Account) Withdraw(amount float64) error {
 	}
 	a.Balance -= amount
 	return nil
+}
+
+func (account *Account) MakeTransactions(commandList []string) string {
+	rejectedTransactions := make(map[int]error)
+	var totalDeposited float64
+	var totalWithdrawn float64
+	var transactionsApplied int
+	for i, line := range commandList {
+		command, amount, err := utils.ParseLine(line)
+		if err != nil {
+			rejectedTransactions[i+1] = err
+		}
+		switch command {
+		case string(Deposit):
+			err := account.Deposit(amount)
+			if err != nil {
+				rejectedTransactions[i+1] = err
+			} else {
+				totalDeposited += amount
+				transactionsApplied++
+			}
+		case string(Withdraw):
+			err := account.Withdraw(amount)
+			if err != nil {
+				rejectedTransactions[i+1] = err
+			} else {
+				totalWithdrawn += amount
+				transactionsApplied++
+			}
+		default:
+			rejectedTransactions[i+1] = errors.New("unknown command: " + command)
+		}
+	}
+	return account.getTransactionsStat(rejectedTransactions, totalDeposited, totalWithdrawn, transactionsApplied)
+}
+
+func (account *Account) getTransactionsStat(
+	rejectedTransactions map[int]error,
+	deposited float64,
+	withdrawn float64,
+	transactionsApplied int,
+) string {
+	var out strings.Builder
+	fmt.Fprintf(&out, "Final balance: %.2f\n", account.Balance)
+	fmt.Fprintf(&out, "Total deposited: %.2f\n", deposited)
+	fmt.Fprintf(&out, "Total withdrawn: %.2f\n", withdrawn)
+	fmt.Fprintf(&out, "Transactions applied: %d\n", transactionsApplied)
+	fmt.Fprintf(&out, "Transactions rejected: %d\n", len(rejectedTransactions))
+	if len(rejectedTransactions) > 0 {
+		fmt.Fprintln(&out, "Rejected details:")
+		for i, err := range rejectedTransactions {
+			fmt.Fprintf(&out, "%d: %s\n", i, err)
+		}
+	}
+	return out.String()
 }
